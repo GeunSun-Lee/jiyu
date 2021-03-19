bfd01fde927fd256816257e4757141fde8e15af0
Geun Sun, Lee <kumdojang@nate.com>
2021-03-19 09:41:21 +0900

    This is a math program

---
+++ math/math.c
@@ -0,0 +1,154 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define ESC 27
+#define COUNT 20
+#define RESULT_TRUE 1
+#define RESULT_FALSE 0
+
+typedef enum OP_TYPE {
+	OP_TYPE_PLUS = 0,
+	OP_TYPE_MINUS,
+	OP_TYPE_MULTIPLE,
+	OP_TYPE_MAX,
+} OP_TYPE;
+
+int check_result(int op, int x, int y, int val)
+{
+	int result = 0;
+
+	switch (op) {
+	case OP_TYPE_PLUS:
+		result = ((x + y) == val) ? RESULT_TRUE : RESULT_FALSE;
+		print_result(result);
+		break;
+	case OP_TYPE_MINUS:
+		result = ((x - y) == val) ? RESULT_TRUE : RESULT_FALSE;
+		print_result(result);
+		break;
+	case OP_TYPE_MULTIPLE:
+		result = ((x * y) == val) ? RESULT_TRUE : RESULT_FALSE;
+		print_result(result);
+		break;
+	defult:
+		printf("[ERR] type error: %d\n", op);
+		return -1;
+	}
+
+	return result;
+}
+
+int print_result(int result)
+{
+	if (result) {
+		printf("%c[1;32m", ESC);
+		printf("  [ O ] Correct");
+		printf("%c[0m\n", ESC);
+		return 0;
+	} else {
+		printf("%c[1;31m", ESC);
+		printf("  [ X ] Incorrect");
+		printf("%c[0m\n", ESC);
+		return -1;
+	}
+}
+
+int save_file(int score)
+{
+	FILE *fp = NULL;
+	time_t t = time(NULL);
+	struct tm tm = *localtime(&t);
+
+	fp = fopen("score.txt", "at");
+	if (fp == NULL) {
+		printf("[ERR] Failed to open a file\n");
+		return -1;
+	}
+
+	fprintf(fp, "%d.%02d.%02d %02d:%02d\tScore: %d\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, score);
+	fclose(fp);
+
+	return 0;
+}
+
+int main() 
+{
+	int i = 0, x = 0, y = 0;
+	int val = 0, score = 0;
+	int ret = 0, tmp = 0;
+	int op = OP_TYPE_MAX;
+
+	srand((unsigned int) time(NULL));
+
+	printf("=======================================\n");
+	printf(" JIYU's\n");
+	printf("\tThe First Math Program\n");
+	printf("=======================================\n");
+
+	printf("Please input right answer.\n");
+
+	while (i < COUNT) {
+		op = rand() % OP_TYPE_MAX;
+
+		switch (op) {
+		case OP_TYPE_PLUS:
+			x = rand() % 100;
+			y = rand() % 100;
+
+			printf(" (%d) %d + %d = ", i + 1, x, y);
+			scanf("%d", &val);
+
+			ret = check_result(op, x, y, val);
+			if (ret)
+				score++;
+			break;
+		case OP_TYPE_MINUS:
+			x = rand() % 100;
+			y = rand() % 100;
+
+			if (x < y) { 
+				tmp = x;
+				x = y;
+				y = tmp;
+			}
+
+			printf(" (%d) %d - %d = ", i + 1, x, y);
+			scanf("%d", &val);
+
+			ret = check_result(op, x, y, val);
+			if (ret)
+				score++;
+			break;
+		case OP_TYPE_MULTIPLE:
+			x = rand() % 10;
+			y = rand() % 10;
+
+			if (x == 0) x = 1;
+			if (y == 0) y = 1;
+
+			printf(" (%d) %d X %d = ", i + 1, x, y);
+			scanf("%d", &val);
+
+			ret = check_result(op, x, y, val);
+			if (ret) 
+				score++;
+			break;
+		default:
+			printf("op type: %d\n", op);
+		}
+
+		i++;
+		printf("\n");
+	}
+
+	printf(" ==================================\n");
+	printf(" \tTotal Score: [ %c[1;31m %d %c[0m ]\n", ESC, score * 5, ESC);
+	printf(" ==================================\n");
+
+	save_file(score);
+
+	return 0;
+}
